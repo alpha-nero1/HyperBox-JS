a44a0d6eba1f1e2d579a4a5af2397fc0ceb515f9
Alessandro Alberga <ale.alberga1@gmail.com>
2021-04-03 12:37:10 +1100

    Mods

--- start-box-server.js
+++ start-box-server.js
@@ -5,11 +5,13 @@ const port = process.env.PORT || 2021;
 
 module.exports = (dir) => {
   const app = express();
+  const srcDir = dir + '/src';
+  const pubDir = dir + '/public'
   console.log(`HyperBox: starting up application...`)
-  app.use(favicon(dir + '../public/favicon.ico'));
-  app.use(express.static(dir));// send the user to index html page inspite of the url
+  app.use(favicon(pubDir + '/favicon.ico'));
+  app.use(express.static(srcDir));// send the user to index html page inspite of the url
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(dir, 'index.html'));
+    res.sendFile(path.resolve(srcDir, 'index.html'));
   });
   app.listen(port, () => {
     console.log(`HyperBox: application running on port ${port} 🚀`)
