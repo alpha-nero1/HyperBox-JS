b8e16a207171e77541cf2588af893a37d0512ec9
Alessandro Alberga <ale.alberga1@gmail.com>
2021-04-05 11:26:18 +1000

    Dist

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,39 @@ module.exports = {
       type: 'umd'
     }
   },
+  externals: {
+    'express-favicon': {
+      commonjs: 'express-favicon',
+      commonjs2: 'express-favicon',
+      amd: 'express-favicon',
+      root: 'express-favicon',
+    },
+    path: {
+      commonjs: 'path',
+      commonjs2: 'path',
+      amd: 'path',
+      root: 'path',
+    },
+    express: {
+      commonjs: 'express',
+      commonjs2: 'express',
+      amd: 'express',
+      root: 'express',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  }
 };
